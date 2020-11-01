a02b9b2bebff491aa144c9d6c91600d038786202
busyhedg03 <62289323+busyhedg03@users.noreply.github.com>
2020-11-01 15:09:52 +0700

    Добавьте файлы проекта.

---
+++ Dijkstra.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <fstream>
+
+int main() {
+    using namespace std;
+    int arrSize{ 0 };
+    fstream fin("input.txt");
+    int k{ 0 };
+    while (fin >> k) {
+        ++arrSize;
+    }
+    fin.seekg(0, ios_base::beg);
+    int *previous = new int[arrSize]{ 0 };
+    k = 0;
+    while (fin >> previous[k]) {
+        ++k;
+    }
+    int *next = new int[arrSize] { 0 };
+    fin.close();
+    return 0;
+}
+ 
