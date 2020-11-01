0c96c3ba2249435b4d3e4ffc1635c8c6f21f7176
busyhedg03 <62289323+busyhedg03@users.noreply.github.com>
2020-11-01 15:37:42 +0700

    1

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -3,18 +3,24 @@
 
 int main() {
     using namespace std;
+    //Размер массивов
     int arrSize{ 0 };
     fstream fin("input.txt");
-    int k{ 0 };
+    //Временная переменная
+    int k{ 0 }; 
     while (fin >> k) {
         ++arrSize;
     }
+    //Возврат указателя на начало
+    fin.clear();
     fin.seekg(0, ios_base::beg);
+    //Предыдущая перестановка
     int *previous = new int[arrSize]{ 0 };
     k = 0;
-    while (fin >> previous[k]) {
+    while (fin >> previous[k]) { //Считывание перестановки
         ++k;
     }
+    //Следующая перестановка
     int *next = new int[arrSize] { 0 };
     fin.close();
     return 0;
